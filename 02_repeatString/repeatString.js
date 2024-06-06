4fcdb1915c4e9bcd83521f8c63b49d2ddc3eae48
EKlijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-06-06 07:14:34 +0200

    complete 02_repeatString - not tested

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return 'ERROR'
+    }
+    resultString = ''
+    
+    for (let i = 0; i < n; i++) {
+        resultString += string
+    }
+    return resultString
 };
 
 // Do not edit below this line
