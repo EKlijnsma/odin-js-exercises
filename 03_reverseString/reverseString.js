7e03cbb787e685bce0fd86f48531a121af0f51ff
EKlijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-06-06 07:23:20 +0200

    complete 03_reverseString - not tested

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let resultString = ''
+    let n = string.length
+    for (let i = n - 1; i >= 0; i--) {
+        resultString += string[i]
+    }
+    return resultString
 };
 
 // Do not edit below this line
