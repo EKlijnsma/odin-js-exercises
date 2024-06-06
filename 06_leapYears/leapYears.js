2618397d334cfda10eac8887d1f74ec475ac9ae0
EKlijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-06-06 08:33:27 +0200

    complete 06_leapYears - not tested

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) ? true : false
 };
 
 // Do not edit below this line
