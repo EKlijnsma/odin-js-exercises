ed2359c3dfd8e9477c6d7d29212f60c8f0e91e20
EKlijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-06-06 08:04:03 +0200

    complete 04_removeFromArray - not tested

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    resultArray = []
+    for (let item of array) {
+        let match = false;
+        for (let arg of args) {
+            if (item === arg) {
+                match = true;
+                break;
+            }
+        }
+        if (match) {
+            continue;
+        } else {
+            resultArray.push(item)
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
