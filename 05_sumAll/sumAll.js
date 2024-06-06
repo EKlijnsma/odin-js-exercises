64e2b2bf2efdaa6ab4f0b44578d872838c60c04c
EKlijnsma <112609864+EKlijnsma@users.noreply.github.com>
2024-06-06 08:23:41 +0200

    complete 05_sumAll - not tested

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    // input must be number, and must be a positive 
+    if (typeof n1 !== 'number' || typeof n2 !== 'number' || n1 < 0 || n2 < 0) {
+        return 'ERROR'
+    }
+    
+    const start = Math.min(n1, n2)
+    const end = Math.max(n1, n2)
+    let sum = 0
 
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
