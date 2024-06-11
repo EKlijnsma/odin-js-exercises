2c69e5ed5c0ec7217d38606ab1aa6f916c5114ec
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-11 13:36:59 +0200

    solve and test 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return 'OOPS';
+    }
+    
+    let curr = 0
+    let prev = 1 
+    for (let i = 0; i < n; i++) {
+        let tmp = prev + curr;
+        prev = curr
+        curr = tmp 
+    }
+    return curr
 
 };
 
