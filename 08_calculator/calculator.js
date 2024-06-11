c85b814f541f3a3dcf5a5ac748a518a4b179c4bd
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-11 12:58:00 +0200

    solve and test 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((prev, curr) => curr + prev, 0)
+  };
+  
+const multiply = function(arr) {
+  return arr.reduce((prev, curr) => curr * prev, 1)
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+	let fact = 1
+  for (let i = x; i > 0; i--) {
+    fact *= i
+  }
+  return fact
 };
 
 // Do not edit below this line
