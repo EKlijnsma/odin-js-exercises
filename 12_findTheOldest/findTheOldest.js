b8c729364e264cb813e5ca4fed2bfc9cf13766c0
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-11 13:55:00 +0200

    solve and test 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a, b) => {
+        if ((a.yearOfDeath || 2024) - a.yearOfBirth > (b.yearOfDeath || 2024) - b.yearOfBirth) {
+            return -1
+        } else {
+            return 1
+        }
+    })
+    return sortedPeople[0]
 };
 
 // Do not edit below this line
