f8d7bf5a6a7fea5df22f3decc466e8dec97eae38
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-11 13:23:18 +0200

    solve and test 09 palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const original = string
+    .toLowerCase()
+    .split('')
+    .filter((char) => char.charCodeAt(0) >= '0'.charCodeAt(0) && char.charCodeAt(0) <= 'z'.charCodeAt(0))
+    .join("")
 
+    const reversed = original
+    .split('').toReversed().join('')
+
+    return original === reversed
 };
 
 // Do not edit below this line
